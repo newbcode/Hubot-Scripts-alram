1aba87e6c9712b766692180265cc4350fb7f4aa1
Yunchang Kang <codenewb@gmail.com>
2013-12-13 14:48:37 +0900

    미세먼지농도 상태 간소화 + 오늘날씨 서울지역 추가

--- lib/Hubot/Scripts/alram.pm
+++ lib/Hubot/Scripts/alram.pm
@@ -1,6 +1,5 @@
 package Hubot::Scripts::alram;
 
-use 5.010;
 use utf8;
 use strict;
 use warnings;
@@ -8,6 +7,7 @@ use Encode qw(encode decode);
 use Data::Printer;
 use DateTime;
 use AnyEvent::DateTime::Cron;
+use LWP::UserAgent;
 
 sub load {
     my ( $class, $robot ) = @_;
@@ -21,13 +21,13 @@ sub load {
                     my $user = $msg->message->user->{name};
 
                     if ( $user =~ /^misskang/) {
-                
-                        my $gm_msg = "ascii GM\n 2013년 펄 크리스마스 달력 사랑해 주세요!.";
-                        my $ga_msg = '점심시간이네요 다들 맛점 하세요♥ ';
-                        my $gn_msg = '수고 많으셨습니다. 다들 컴백홈!';
-                        my $std_msg = '[0∼30:좋음][31∼80보통][81∼120/민감군 영향][121∼200:나쁨][201∼300:매우나쁨][301∼600:위험]';
 
-                        $cron->add( '*/5 * * * *'  => sub {
+                                       
+                        my $gm_msg = "ascii GM";
+                        my $ga_msg = '즐거운 점심시간 맛점♥';
+                        my $gn_msg = '퇴근 30분전..... 조심히 컴백홈 하세요';
+                       
+                        $cron->add( '*/1 * * * *'  => sub {
                             my $dt = DateTime->now( time_zone => 'Asia/Seoul' );
                             my ($ymd, $year, $month, $day, $hour, $min ) = ($dt->ymd, $dt->year, $dt->month, $dt->day, $dt->hour, $dt->min);
 
@@ -37,18 +37,26 @@ sub load {
                             if ( $min < 10 ) { $min = "0"."$min"; }
 
                             my $now_time = "$ymd".'-'."$hour".':'."$min";
+                            my $yymmdd = "$year$month$day";
+
+                            my $ta_msg = weather($yymmdd);
+
+                            my ($pods_status, $pods_msg) = pods();
 
                             if ( $now_time =~ /^\d\d\d\d\-\d\d\-\d\d\-09:30$/ ) { 
                                 $msg->send($gm_msg); 
-                                pods($msg);
+                            }
+                            elsif ( $now_time =~ /^\d\d\d\d\-\d\d\-\d\d\-10:30$/ ) { 
+                                $msg->send("오늘날씨(서울)[$ta_msg] 미세먼지농도-[$pods_msg($pods_status)]");
                             }
                             elsif ( $now_time =~ /^\d\d\d\d\-\d\d\-\d\d\-11:50$/ ) { 
                                 $msg->send($ga_msg); 
-                                pods($msg);
                             }
-                            elsif ( $now_time =~ /^\d\d\d\d\-\d\d\-\d\d\-18:35$/ ) { 
+                            elsif ( $now_time =~ /^\d\d\d\d\-\d\d\-\d\d\-16:30$/ ) { 
+                                $msg->send("오늘날씨(서울)[$ta_msg] 미세먼지농도-[$pods_msg($pods_status)]");
+                            }
+                            elsif ( $now_time =~ /^\d\d\d\d\-\d\d\-\d\d\-17:45$/ ) { 
                                 $msg->send($gn_msg); 
-                                pods($msg);
                             }
                         }
                     );
@@ -59,30 +67,62 @@ sub load {
 }
 
 sub pods {
-    my $msg = shift;
-    my ($today, $pol, $cai, $cdata);
+    my $ua = LWP::UserAgent->new;
+    my $cdata;
 
-    $msg->http("http://openapi.seoul.go.kr:8088/$ENV{HUBOT_OPENAPI_KEY}/xml/ForecastWarningMinuteParticleOfDustService/1/1/")->get(
-            sub {
-                my ( $body, $hdr ) = @_;
-                return if ( !$body || $hdr->{Status} !~ /^2/ );
-                my $decode_body = decode ( 'UTF-8', $body ); 
+    my $url = "http://openapi.seoul.go.kr:8088/$ENV{HUBOT_OPENAPI_KEY}/xml/ForecastWarningMinuteParticleOfDustService/1/1/";
+    my $resp = $ua->get($url);
 
-                if ( $decode_body =~ /<APPLC_DT>(\d+)<\/APPLC_DT>/ ) {
-                    $today = $1;
-                }
-                if ( $decode_body =~ /<POLLUTANT>(.*?)<\/POLLUTANT>/ ) {
-                    $pol = $1;
-                }
-                if ( $decode_body =~ /<CAISTEP>(.*?)<\/CAISTEP>/ ) {
-                    $cai = $1;
-                }
-                if ( $decode_body =~ /<ALARM_CNDT><!\[CDATA\[(.+)/ ) {
-                    $cdata = $1;
-                }
-            $msg->send("[미세먼지 예보=> 오염물질-$pol\[$cai\] 미세먼지 농도-$cdata]"); 
-            }
+    my %status = (
+        '0~30'         => '좋음',
+        '31~80'        => '보통',
+        '81~120'       => '민감군 영향',
+        '121~200'      => '나쁨',
+        '201~300'      => '매우나쁨',
+        '301~600'      => '위험',
     );
+
+        if ($resp->is_success) {
+            my $decode_body = $resp->decoded_content;
+
+            if ( $decode_body =~ /<ALARM_CNDT><!\[CDATA\[.*?(\d+~\d+)/ ) {
+                $cdata = $1;
+                foreach my $num ( keys %status ) {
+                    if ($num eq $cdata) {
+                        return ($num, $status{$num}); 
+                    }
+                }
+            }
+        }
+        else {
+            die $resp->status_line;
+        }
+}
+
+sub weather {
+    my $today = shift;
+    my ($ta_min, $ta_max);
+
+    my $ua = LWP::UserAgent->new;
+    my $url = "http://openapi.seoul.go.kr:8088/$ENV{HUBOT_OPENAPI_KEY}/xml/DailyWeatherStation/1/5/$today";
+    my $resp = $ua->get($url);
+
+    if ($resp->is_success) {
+
+        my $decode_body =  $resp->decoded_content;
+
+        if ( $decode_body =~ /<SAWS_TA_MIN>(.+)<\/SAWS_TA_MIN>/ ) {
+            $ta_min = $1;
+        }
+        if ( $decode_body =~ /<SAWS_TA_MAX>(.+)<\/SAWS_TA_MAX>/ ) {
+            $ta_max = $1;
+        }
+    return ("$ta_min/$ta_max");
+
+    }
+    else {
+        die $resp->status_line;
+    }
 }
 
 1;
